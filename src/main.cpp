1f3d8f5fb709bfd620f9fdda55b8de1a5875790d
Aleksa <aleksa.danic.2005@gmail.com>
2026-06-09 13:25:30 -0700

    confused .s with .S - fixed

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "../h/MemoryAllocator.hpp"
 
 extern "C" {
-    void interruptRoutine();
+    void interruptRoutine ();
 
     uint64 handleInterrupt (uint64 syscallNum, uint64 a1, uint64 a2, uint64 a3) {
         switch (syscallNum) {
@@ -12,6 +12,7 @@ extern "C" {
             case 0x02:
                 return (uint64) MemoryAllocator::getInstance().free((void*) a1);
         }
+        return (uint64) -1;
     }
 }
 
